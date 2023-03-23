5b0cc098dce841e2ea5e07915e18d86d780ef90a
sharan2807 <sharancold98@gmail.com>
2023-03-23 12:44:48 +0530

    task5 1st commit

---
+++ Task4/task5.js
@@ -0,0 +1,12 @@
+// let items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+
+// throws error 
+// items[4].style.backgroundColor='green';
+
+
+let items = document.getElementsByTagName('li');
+console.log(items);
+
+// doesn't throws error 
+items[4].style.backgroundColor='green';
