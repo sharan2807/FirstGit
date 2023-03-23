a2b19f6a9eeb1453000976c8b3590145dc21c308
sharan2807 <sharancold98@gmail.com>
2023-03-23 15:56:12 +0530

    task6 part1

---
+++ Task4/task6.js
@@ -0,0 +1,36 @@
+// QUERRY SELECTOR  
+
+// var header= document.querySelector('#main-header');
+// header.style.borderBottom='solid 4px #ccc';
+
+// var input = document.querySelector('input');
+// input.value ='Hello World'
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value="SEND"
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color ='red';
+
+// var lastItem = document.querySelector
+// ('.list-group-item:last-child');
+// lastItem.style.color='blue';
+
+// var secondItem = document.querySelector
+// ('.list-group-item:nth-child(2)');
+// secondItem.style.color='coral';
+
+// QUERRY SELECTOR ALL
+
+// var odd=document.querySelectorAll('li:nth-child(odd)');
+
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor='green';
+// }
+
+// Task part1:
+var second=document.querySelector('li:nth-child(2)');
+second.style.backgroundColor='green';
+
+var third=document.querySelector('li:nth-child(3)');
+third.style.visibility='hidden';
