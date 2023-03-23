c7edc68655a04af7bb2d6056c378394cf6d99abe
sharan2807 <sharancold98@gmail.com>
2023-03-23 11:43:35 +0530

    1st commit

---
+++ Task4/task4.js
@@ -0,0 +1,13 @@
+let items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+
+// for(var i=0;i<items.length;i++){
+//     items[i].style.backgroundColor='#f4f4f4';
+// }
+
+items[2].style.backgroundColor='green';
+
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
